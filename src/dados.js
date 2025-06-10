82b11997c9575d6d7afb3799dfd33b7f28fe3672
Pedro Feld <pedro.gael.feld@gmail.com>
2025-06-10 20:28:38 -0300

    feat: adição de dados base (dados de pets)

--- src/dados.js
+++ src/dados.js
@@ -1,3 +1,32 @@
 import { randomUUID } from 'crypto';
 
-export const dados = [];
+export const dados = [
+    {
+        id: randomUUID(),
+        nome: 'Atlas',
+        raca: 'Husky Siberiano',
+        idade: 2,
+        nomeTutor: 'Pedro Feld'
+    },
+    {
+        id: randomUUID(),
+        nome: 'Sol',
+        raca: 'Golden Retriever',
+        idade: 3,
+        nomeTutor: 'Pedro Feld'
+    },
+    {
+        id: randomUUID(),
+        nome: 'Toby',
+        raca: 'Yorkshire',
+        idade: 2,
+        nomeTutor: 'Anelise Lanius'
+    },
+    {
+        id: randomUUID(),
+        nome: 'Russo',
+        raca: 'Pit bull',
+        idade: 1,
+        nomeTutor: 'Jessica Vaz'
+    }
+];
